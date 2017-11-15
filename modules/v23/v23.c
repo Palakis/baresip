40f7b4e7b29451f5443e6dd8cb454aeed4dba43e
Stéphane Lepin <stephane.lepin@gmail.com>
2017-11-16 00:27:01 +0100

    shared common state

--- modules/v23/v23.c
+++ modules/v23/v23.c
@@ -12,44 +12,65 @@
  * NOTE: very experimental
  */
 
-static void set_sine(struct v23modem_enc* st, uint32_t freq, int amplitude) {
-	autone_sine(st->tone_buf, st->srate, freq, amplitude, 0, 0);
-	mbuf_set_pos(st->tone_buf, 0);
+static void state_destructor(void *arg)
+{
+	struct v23_t *st = (struct v23_t*)arg;
+	(void)st;
 }
 
 static void enc_destructor(void *arg)
 {
-	struct v23modem_enc *st = arg;
+	struct v23modem_enc *st = (struct v23modem_enc*)arg;
 	mem_deref(st->tone_buf);
+	mem_deref(st->v23);
 }
 
 static void dec_destructor(void *arg)
 {
-	struct v23modem_dec *st = arg;
+	struct v23modem_dec *st = (struct v23modem_dec*)arg;
 	(void)st;
+}
+
+static void set_sine(struct v23modem_enc* st, uint32_t freq, int amplitude) {
+	autone_sine(st->tone_buf, st->srate, freq, amplitude, 0, 0);
+	mbuf_set_pos(st->tone_buf, 0);
 }
 
 static int encode_update(struct aufilt_enc_st **stp, void **ctx,
 						 const struct aufilt *af, struct aufilt_prm *prm)
 {
 	struct v23modem_enc *st;
-	(void)ctx;
+	struct v23_t *common_st;
 
 	if (!stp || !af || !prm)
 		return EINVAL;
+
+	common_st = mem_zalloc(sizeof(*common_st), state_destructor);
+	if (!common_st)
+		return ENOMEM;
 
 	st = mem_zalloc(sizeof(*st), enc_destructor);
 	if (!st)
 		return ENOMEM;
 
+	st->v23 = common_st;
+
 	st->srate = prm->srate;
 	st->tone_buf = mbuf_alloc(st->srate * 2);
 	st->ns_counter = 0;
 
-	st->conn_state = V23_HOOK;
+	st->v23->conn_state = V23_HOOK;
 	st->hook_state = HOOK_NONE;
 
 	*stp = (struct aufilt_enc_st*)st;
+
+	// encode_update is called before decode_update in
+	// in the same scope, with ctx's value accessible from
+	// these two handlers.
+	// It's then possible to initialize a common context in
+	// encode_update, store its pointer in ctx, and fetch it
+	// again in decode_update.
+	*ctx = common_st;
 
 	return 0;
 }
@@ -58,7 +79,6 @@ static int decode_update(struct aufilt_dec_st **stp, void **ctx,
 						 const struct aufilt *af, struct aufilt_prm *prm)
 {
 	struct v23modem_dec *st;
-	(void)ctx;
 
 	if (!stp || !af || !prm)
 		return EINVAL;
@@ -67,6 +87,7 @@ static int decode_update(struct aufilt_dec_st **stp, void **ctx,
 	if (!st)
 		return ENOMEM;
 
+	st->v23 = (struct v23_t*)ctx;
 	st->srate = prm->srate;
 
 	*stp = (struct aufilt_dec_st*)st;
@@ -84,7 +105,7 @@ static int encode(struct aufilt_enc_st *st, int16_t *sampv, size_t *sampc)
 	size_t i, samples = *sampc;
 
 	for(i = 0; i < samples; i++) {
-		if (ctx->conn_state == V23_HOOK) {
+		if (ctx->v23->conn_state == V23_HOOK) {
 			if (ctx->hook_state == HOOK_FREQ_2100 && ctx->ns_counter >= ms_to_ns(3000)) {
 				ctx->hook_state = HOOK_SILENCE;
 				set_sine(ctx, 0, 0);
--- modules/v23/v23.h
+++ modules/v23/v23.h
@@ -11,16 +11,21 @@ enum v23_connection_state {
     V23_DISCONNECTED
 };
 
+struct v23_t {
+    enum v23_connection_state conn_state;
+};
+
 struct v23modem_enc {
     struct aufilt_enc_st af; // base class
+    struct v23_t *v23;
     uint32_t srate;
     struct mbuf *tone_buf;
     uint64_t ns_counter;
-    enum v23_connection_state conn_state;
     enum v23_hook_sequence_state hook_state;
 };
 
 struct v23modem_dec {
     struct aufilt_dec_st af; // base class
+    struct v23_t *v23;
     uint32_t srate;
 };
